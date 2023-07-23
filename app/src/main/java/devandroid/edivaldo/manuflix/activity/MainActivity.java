5b1106899c20a781cb031347b3efdaea0e5229d7
edivaldobeserra <97808328+edivaldobeserra@users.noreply.github.com>
2023-07-23 10:22:40 -0300

    Splash activity

---
+++ app/src/main/java/devandroid/edivaldo/manuflix/activity/MainActivity.java
@@ -0,0 +1,29 @@
+package devandroid.edivaldo.manuflix.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import devandroid.edivaldo.manuflix.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+
+        NavigationUI.setupWithNavController(bottomNavigationView,navController);
+    }
+}
