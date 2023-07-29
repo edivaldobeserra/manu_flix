2aa6a32368f558327887ea2f43ef0a520919933a
edivaldobeserra <97808328+edivaldobeserra@users.noreply.github.com>
2023-07-29 17:04:25 -0300

    layout cadastro

---
+++ app/src/main/java/devandroid/edivaldo/manuflix/autenticacao/CadastroActivity.java
@@ -0,0 +1,16 @@
+package devandroid.edivaldo.manuflix.autenticacao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import devandroid.edivaldo.manuflix.R;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
--- app/src/main/java/devandroid/edivaldo/manuflix/autenticacao/LoginActivity.java
+++ app/src/main/java/devandroid/edivaldo/manuflix/autenticacao/LoginActivity.java
@@ -2,6 +2,7 @@ package devandroid.edivaldo.manuflix.autenticacao;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import devandroid.edivaldo.manuflix.R;
@@ -12,5 +13,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        configClicks();
+    }
+
+    public void configClicks(){
+        findViewById(R.id.btn_cadastro).setOnClickListener(view ->
+                startActivity(new Intent(this, CadastroActivity.class)));
     }
 }
