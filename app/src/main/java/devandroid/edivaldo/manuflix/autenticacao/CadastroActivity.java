098f6f8d501c4f03868ef96fc62f888630d073af
edyen <edivaldobeserra023@gmail.com>
2023-08-03 11:28:15 -0300

    validando dados

--- app/src/main/java/devandroid/edivaldo/manuflix/autenticacao/CadastroActivity.java
+++ app/src/main/java/devandroid/edivaldo/manuflix/autenticacao/CadastroActivity.java
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 import devandroid.edivaldo.manuflix.R;
+import devandroid.edivaldo.manuflix.activity.MainActivity;
+import devandroid.edivaldo.manuflix.helper.FirebaseHelper;
 
 public class CadastroActivity extends AppCompatActivity {
     private EditText edtEmail;
@@ -19,6 +23,7 @@ public class CadastroActivity extends AppCompatActivity {
 
         iniciaComponentes();
         configClicks();
+
     }
 
     private void validaDados() {
@@ -29,6 +34,8 @@ public class CadastroActivity extends AppCompatActivity {
             if (!senha.isEmpty()) {
 
                 cadastroFirebase(email, senha);
+
+                
 
         } else {
             edtSenha.requestFocus();
@@ -43,12 +50,27 @@ public class CadastroActivity extends AppCompatActivity {
     }
 
     private void cadastroFirebase(String email, String senha) {
+        FirebaseHelper.getAuth().createUserWithEmailAndPassword(
+                email, senha
+        ).addOnCompleteListener(task -> {
+            if (task.isSuccessful()){
+                finish();
+                startActivity(new Intent(this, MainActivity.class));
+
+            }else {
+                Toast.makeText(this, FirebaseHelper.validaErros(task.getException().getMessage()),Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
 
 
     }
 
-    public void configClicks() {
+    private void configClicks() {
         findViewById(R.id.btn_Login).setOnClickListener(view -> finish());
+
+        findViewById(R.id.btnCadastro).setOnClickListener(view -> validaDados());
     }
 
     private void iniciaComponentes() {
