40cb0759cefb1f220ec27450ab6d89ddca5c2715
edivaldobeserra <97808328+edivaldobeserra@users.noreply.github.com>
2023-08-08 21:54:46 -0300

    Carregando imagens da galeria

--- app/src/main/java/devandroid/edivaldo/manuflix/fragments/AddFragment.java
+++ app/src/main/java/devandroid/edivaldo/manuflix/fragments/AddFragment.java
@@ -1,19 +1,35 @@
 package devandroid.edivaldo.manuflix.fragments;
 
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.gun0912.tedpermission.PermissionListener;
+
+import com.gun0912.tedpermission.normal.TedPermission;
+
+import java.util.List;
 
 import devandroid.edivaldo.manuflix.R;
+import devandroid.edivaldo.manuflix.activity.MainActivity;
 
 
 public class AddFragment extends Fragment {
+    private final int SELECAO_GALERIA = 100;
+
+    private ImageView imageView;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,5 +41,55 @@ public class AddFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        iniciaComponentes(view);
+        congigClick();
+    }
+    private void congigClick(){
+        imageView.setOnClickListener(v -> verificarPermissaoGaleria());}
+
+    private void verificarPermissaoGaleria(){
+        PermissionListener permissionlistener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                abrirGaleria();
+
+            }
+
+            @Override
+            public void onPermissionDenied(List<String> deniedPermissions) {
+                Toast.makeText(getContext(),"Permissão Negada", Toast.LENGTH_SHORT).show();
+
+            }
+
+        };
+
+        TedPermission.create()
+                .setPermissionListener(permissionlistener)
+                .setDeniedTitle("Permissões")
+                .setDeniedMessage("Se você não aceitar a permissão não poderá acessar a Galeria do dispositivo, deseja ativar a permissão agora ?")
+                .setDeniedCloseButtonText("Não")
+                .setGotoSettingButtonText("Sim")
+                .setPermissions(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .check();
+
+
+    }
+
+    private void abrirGaleria(){
+        Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, SELECAO_GALERIA);
+
+
+    }
+
+
+    private void iniciaComponentes(View view){
+        imageView = view.findViewById(R.id.imageV);
+
+
+    }
+
+    private class READ_EXTERNAL_STORAGE {
     }
 }
