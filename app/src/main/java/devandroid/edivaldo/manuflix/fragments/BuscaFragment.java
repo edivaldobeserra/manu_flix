a24a28f05a76837f67460a00eceb6f5ebe7f1510
edivaldobeserra <97808328+edivaldobeserra@users.noreply.github.com>
2023-09-03 19:34:21 -0300

    Finalizando a activity de busca

--- app/src/main/java/devandroid/edivaldo/manuflix/fragments/BuscaFragment.java
+++ app/src/main/java/devandroid/edivaldo/manuflix/fragments/BuscaFragment.java
@@ -1,5 +1,6 @@
 package devandroid.edivaldo.manuflix.fragments;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,8 +13,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -22,6 +26,7 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import devandroid.edivaldo.manuflix.R;
 import devandroid.edivaldo.manuflix.adapter.AdapterBusca;
@@ -31,12 +36,13 @@ import devandroid.edivaldo.manuflix.model.Post;
 
 
 public class BuscaFragment extends Fragment {
+
+    private List<Post> postList = new ArrayList<>();
     private SearchView searchView;
     private RecyclerView rvPosts;
     private ProgressBar progressBar;
     private TextView textInfo;
     private AdapterBusca adapterBusca;
-    private List<Post> postList = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -52,11 +58,68 @@ public class BuscaFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         iniciaComponentes(view);
-        configRv();
+        configRv(postList);
         recuperaPost();
+        configSearchView();
     }
 
-    private void recuperaPost(){
+    private void configSearchView() {
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String pesquisa) {
+                if (pesquisa.length() >= 3) {
+
+
+                    buscarPosts(pesquisa);
+
+                } else {
+                    ocultarTeclado();
+                    Toast.makeText(getContext(), "Mínimo 3 caracteres", Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        searchView.findViewById(androidx.appcompat.R.id.search_close_btn).setOnClickListener(view -> {
+            ocultarTeclado();
+            recuperaPost();
+
+            EditText edt = searchView.findViewById( androidx.appcompat.R.id.search_src_text);
+            edt.getText().clear();
+
+            searchView.clearFocus();
+        });
+
+    }
+
+
+
+    private void buscarPosts(String pesuisa) {
+
+        List<Post> postListBusca = new ArrayList<>();
+
+
+        for (Post post : postList) {
+            if (post.getTitulo().toUpperCase(Locale.ROOT).contains(pesuisa.toUpperCase(Locale.ROOT))) {
+                postListBusca.add(post);
+            }
+        }
+        if (!postListBusca.isEmpty()) {
+            configRv(postListBusca);
+        } else {
+            configRv(new ArrayList<>());
+            textInfo.setText("Nenhum post encontrado com o nome pesuisado.");
+
+        }
+    }
+
+    private void recuperaPost() {
+
+
         DatabaseReference postRef = FirebaseHelper.getDatabaseReference()
                 .child("posts");
         postRef.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -64,19 +127,20 @@ public class BuscaFragment extends Fragment {
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 if (snapshot.exists()) {
                     postList.clear();
-                    for (DataSnapshot ds : snapshot.getChildren()){
+                    for (DataSnapshot ds : snapshot.getChildren()) {
                         Post post = ds.getValue(Post.class);
                         postList.add(post);
 
                     }
-
+                    textInfo.setText("");
                 } else {
                     textInfo.setText("Nenhum post cadastrado");
 
                 }
 
                 progressBar.setVisibility(View.GONE);
-                adapterBusca.notifyDataSetChanged();
+                configRv(postList);
+
             }
 
             @Override
@@ -87,18 +151,25 @@ public class BuscaFragment extends Fragment {
 
     }
 
-    private void configRv(){
+    private void configRv(List<Post> postList) {
         rvPosts.setLayoutManager(new LinearLayoutManager(getContext()));
         rvPosts.setHasFixedSize(true);
         adapterBusca = new AdapterBusca(postList, getContext());
         rvPosts.setAdapter(adapterBusca);
+        adapterBusca.notifyDataSetChanged();
     }
 
-    private void iniciaComponentes(View view){
+    private void iniciaComponentes( View view) {
         searchView = view.findViewById(R.id.searchView);
         rvPosts = view.findViewById(R.id.rvPosts);
         progressBar = view.findViewById(R.id.progressBar);
         textInfo = view.findViewById(R.id.textInfo);
+
+    }
+
+    private void ocultarTeclado() {
+        InputMethodManager inputMethodManager = (InputMethodManager) getContext().getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(searchView.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
 
     }
 }
